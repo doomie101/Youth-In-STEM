07c7f0d442a6076c22722f1fc94af2fccab99bbb
doomie101 <brightaquilla@gmail.com>
2023-02-19 22:51:53 +0300

    end of week 2 code

--- week-2/string.py
+++ week-2/string.py
@@ -1,14 +1,11 @@
 
-poem = """ this is a poem about nothing
-             its funny people laugh about
-print (poem)
-"""
 
-age =25
-f_name= "Bright"
+
+
+f_name = "Bright"
 s_name = "Aquilla"
 full_name = f_name + s_name
 
-
-print ("my name is {} and i am {} years old".format(full_name, age))
-print(" Hello mary \n How are you \n I am fine")
+age = 25 #years
+print("my name is {} and i am {} years old".format(full_name, age))
+print(" Hello from Bright \t how are you \t Iam fine")
